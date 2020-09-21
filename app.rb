10871a56fffc15a76cd590be3404537cbeae5a35
lenin <lenin.montalvo.23@hotmail.com>
2020-09-21 10:42:06 -0500

    Add bubble_sort method

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
-require 'rubygems'
-require 'bundler/setup'
+def bubble_sort(array)
+  array.sort
+end
+
+def bubble_sort_by(ary)
+  
+  {|a, b| b <=> a}
+end
+
+p bubble_sort_by(["hi", "hey", "hello"])
+
+
+
