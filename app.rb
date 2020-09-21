23caae61080bd9629513b319588d518b1170a1cc
unknown <martin_najjar@yahoo.com>
2020-09-21 23:00:21 +0300

    Indent the lines correctly

--- app.rb
+++ app.rb
@@ -1,34 +1,32 @@
-
 def bubble_sort(array)
-  sorted_array = array;
- array.map.with_index do |val,indx|
-    i=0
-    if (indx < array.length)
-      while i < array.length-1
-       
-        if(array[i] > array[i+1])
-          next_value = array[i+1]
-          array[i+1] = array[i]
+  sorted_array = array
+  array.map.with_index do |_val, indx|
+    i = 0
+    if indx < array.length
+      while i < array.length - 1
+
+        if array[i] > array[i + 1]
+          next_value = array[i + 1]
+          array[i + 1] = array[i]
           array[i] = next_value
           sorted_array = array
         end
         i += 1
       end
-    
     end
   end
   sorted_array
 end
 
 def bubble_sort_by(array)
-  sorted_array = array;
- array.map.with_index do |val,indx|
-    i=0
-    if (indx < array.length)
-      while i < array.length-1
-        if(yield(array[i], array[i+1])==1)
-          next_value = array[i+1]
-          array[i+1] = array[i]
+  sorted_array = array
+  array.map.with_index do |_val, indx|
+    i = 0
+    if indx < array.length
+      while i < array.length - 1
+        if yield(array[i], array[i + 1]) == 1
+          next_value = array[i + 1]
+          array[i + 1] = array[i]
           array[i] = next_value
           sorted_array = array
         end
@@ -39,17 +37,13 @@ def bubble_sort_by(array)
   sorted_array
 end
 
-
-new_arr = bubble_sort_by(%w[hello hey hi here]) do
-  |left,right|
-   result = left.length - right.length
-   if(result.negative?) 
-     -1
-   elsif (result.positive?)
-      1
-   else
-      0
-   end
+bubble_sort_by(%w[hello hey hi here]) do |left, right|
+  result = left.length - right.length
+  if result.negative?
+    -1
+  elsif result.positive?
+    1
+  else
+    0
+  end
 end
-
-
